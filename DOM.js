8e61d58bfb68086ad1274636e0b83c73f75d1d8e
karen <karenwanngugi@gmail.com>
2025-06-03 11:03:00 +0300

    DOM assignment

--- DOM.js
+++ DOM.js
@@ -14,6 +14,7 @@ const headings = document.querySelectorAll('h3');
  const newFruit = document.createElement('li')
  newFruit.textContent ='Orange'
  fruitList.appendChild(newFruit)
+ 
  const img1 = document.createElement('img');
  img1.src = 'orange.jpeg'
  img1.height = 200
@@ -26,6 +27,7 @@ const headings = document.querySelectorAll('h3');
  const newVeggie = document.createElement('li')
  newVeggie.textContent = 'Cabbage'
  vegList.appendChild(newVeggie)
+
  const img = document.createElement('img')
  img.src = 'cabbage.webp'
  img.height = 200
